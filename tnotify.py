47c45511782867d163fabece96615fb2cad7ac98
NP-Hardass <np.hardass@gmail.com>
2020-04-01 20:34:15 -0400

    tnotify.py: update to have py3 support with futurize

--- tnotify.py
+++ tnotify.py
@@ -38,6 +38,7 @@ from os import environ, path, kill, rename, remove
 import signal
 import hashlib
 import time
+from builtins import object
 
 SCRIPT_NAME = "tnotify"
 SCRIPT_VERSION = "0.1.0"
@@ -98,7 +99,7 @@ class config(object):
 		self.verify_qt()
 
 	def init_config(self):
-		for opt, value in self.opts.items():
+		for opt, value in list(self.opts.items()):
 			temp = weechat.config_get_plugin(opt)
 			if not len(temp):
 				weechat.config_set_plugin(opt, value)
@@ -185,13 +186,13 @@ def config_cb(data, option, value):
 
 def bootstrap_tray():
 	BUF_SIZE = 65536
-	weechat.mkdir_parents(plugin_dir, 0755)
+	weechat.mkdir_parents(plugin_dir, 0o755)
 
 	if weechat.config_get_plugin("icon") == "weechat":
 		# Download icon
 		icon = plugin_dir + "/icons/weechat.png"
 		if not path.exists( icon ):
-			weechat.mkdir_parents( plugin_dir + "/icons" , 0755)
+			weechat.mkdir_parents( plugin_dir + "/icons" , 0o755)
 			weechat.hook_process_hashtable("url:" + WEECHAT_ICON_URL,
 					{"file_out": icon + ".tmp"}, 30 * 1000, "process_download_cb", "")
 			time.sleep(0.5)
@@ -208,7 +209,7 @@ def bootstrap_tray():
 	if weechat.config_get_plugin("alt-icon") == "null":
 		icon = plugin_dir + "/icons/null.png"
 		if not path.exists( icon ):
-			weechat.mkdir_parents( plugin_dir + "/icons" , 0755)
+			weechat.mkdir_parents( plugin_dir + "/icons" , 0o755)
 			weechat.hook_process_hashtable("url:" + NULL_ICON_URL,
 					{"file_out": icon + ".tmp"}, 30 * 1000, "process_download_cb", "")
 			time.sleep(0.5)
